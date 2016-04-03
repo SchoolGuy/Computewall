34f66110e18cface7b7d13a705553720c5c015ba
SchoolGuy <matrixfueller@gmail.com>
2016-04-03 09:10:44 +0200

    Mathematical solution for this problem. Testing not Successful.

--- src/Main/computewall.java
+++ src/Main/computewall.java
@@ -2,14 +2,12 @@ package Main;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Random;
 
 public class computewall {
 
     int[][] computewallInt = null;
     int count;
     ArrayList<Integer> sortedComputewall = null;
-    Random rnd = new Random();
 
     public computewall() {
         count = 0;
@@ -40,60 +38,109 @@ public class computewall {
     }
 
     public void calculate() {
-        boolean uniqueCheck;
-        do {
-            count++;
-            if (count % 1000 == 0) {
-                System.out.println("count: " + count);
-            }
-            sortedComputewall.clear();
-            uniqueCheck = false;
-            //Set values
-            for (int i = 0; i < 4; i++) {
-                for (int j = 0; i < 16; j++) {
-                    computewallValueReplace(computewallInt, 4, i, j);
+        boolean unique = false;
+        //Set values
+        for (int i = 1; i < 16 & !unique; i++) {
+            for (int j = 1; j < 16 & !unique; j++) {
+                for (int k = 1; k < 16 & !unique; k++) {
+                    for (int l = 1; l < 16 & !unique; l++)
+                        for (int m = 1; m < 16 & !unique; m++) {
+                            unique = false;
+                            System.out.println(i + "|" + j + "|" + k + "|" + l + "|" + m);
+                            if (!(i==j | i==k | i==l | i==m | j==k | j==m | j==l | k==l | k==m | l==m)) {
+                                computewallInt [4][0] = i;
+                                computewallInt [4][1] = j;
+                                computewallInt [4][2] = k;
+                                computewallInt [4][3] = l;
+                                computewallInt [4][4] = m;
+
+                                //Rest of calculating
+                                computewallInt[3][0] = Math.abs(computewallInt[4][0] - computewallInt[4][1]);
+                                computewallInt[3][1] = Math.abs(computewallInt[4][1] - computewallInt[4][2]);
+                                computewallInt[3][2] = Math.abs(computewallInt[4][2] - computewallInt[4][3]);
+                                computewallInt[3][3] = Math.abs(computewallInt[4][3] - computewallInt[4][4]);
+                                computewallInt[2][0] = Math.abs(computewallInt[3][0] - computewallInt[3][1]);
+                                computewallInt[2][1] = Math.abs(computewallInt[3][1] - computewallInt[3][2]);
+                                computewallInt[2][2] = Math.abs(computewallInt[3][2] - computewallInt[3][3]);
+                                computewallInt[1][0] = Math.abs(computewallInt[2][0] - computewallInt[2][1]);
+                                computewallInt[1][1] = Math.abs(computewallInt[2][1] - computewallInt[2][2]);
+                                computewallInt[0][0] = Math.abs(computewallInt[1][0] - computewallInt[1][1]);
+                                sortedComputewall.add(computewallInt[0][0]);
+                                sortedComputewall.add(computewallInt[1][0]);
+                                sortedComputewall.add(computewallInt[1][1]);
+                                sortedComputewall.add(computewallInt[2][0]);
+                                sortedComputewall.add(computewallInt[2][1]);
+                                sortedComputewall.add(computewallInt[2][2]);
+                                sortedComputewall.add(computewallInt[3][0]);
+                                sortedComputewall.add(computewallInt[3][1]);
+                                sortedComputewall.add(computewallInt[3][2]);
+                                sortedComputewall.add(computewallInt[3][3]);
+                                sortedComputewall.add(computewallInt[3][3]);
+                                sortedComputewall.add(computewallInt[4][4]);
+                                sortedComputewall.add(computewallInt[4][1]);
+                                sortedComputewall.add(computewallInt[4][2]);
+                                sortedComputewall.add(computewallInt[4][3]);
+                                Object[] sortedComputeWallInt = sortedComputewall.toArray();
+                                Arrays.sort(sortedComputeWallInt);
+                                if (!sortedComputewall.contains(0) & sortedComputewall.contains(1) & sortedComputewall.contains(2) &
+                                        sortedComputewall.contains(3) & sortedComputewall.contains(4) & sortedComputewall.contains(5) & sortedComputewall.contains(6) &
+                                        sortedComputewall.contains(7) & sortedComputewall.contains(8) & sortedComputewall.contains(9) &
+                                        sortedComputewall.contains(10) & sortedComputewall.contains(11) & sortedComputewall.contains(12) & sortedComputewall.contains(13) &
+                                        sortedComputewall.contains(14) & sortedComputewall.contains(15)) {
+                                    unique = true;
+                                }
+                            }
+                        }
                 }
             }
+        }
 
-            //Rest of calculating
-            computewallInt[3][0] = Math.abs(computewallInt[4][0] - computewallInt[4][1]);
-            computewallInt[3][1] = Math.abs(computewallInt[4][1] - computewallInt[4][2]);
-            computewallInt[3][2] = Math.abs(computewallInt[4][2] - computewallInt[4][3]);
-            computewallInt[3][3] = Math.abs(computewallInt[4][3] - computewallInt[4][4]);
-            computewallInt[2][0] = Math.abs(computewallInt[3][0] - computewallInt[3][1]);
-            computewallInt[2][1] = Math.abs(computewallInt[3][1] - computewallInt[3][2]);
-            computewallInt[2][2] = Math.abs(computewallInt[3][2] - computewallInt[3][3]);
-            computewallInt[1][0] = Math.abs(computewallInt[2][0] - computewallInt[2][1]);
-            computewallInt[1][1] = Math.abs(computewallInt[2][1] - computewallInt[2][2]);
-            computewallInt[0][0] = Math.abs(computewallInt[1][0] - computewallInt[1][1]);
-            sortedComputewall.add(computewallInt[0][0]);
-            sortedComputewall.add(computewallInt[1][0]);
-            sortedComputewall.add(computewallInt[1][1]);
-            sortedComputewall.add(computewallInt[2][0]);
-            sortedComputewall.add(computewallInt[2][1]);
-            sortedComputewall.add(computewallInt[2][2]);
-            sortedComputewall.add(computewallInt[3][0]);
-            sortedComputewall.add(computewallInt[3][1]);
-            sortedComputewall.add(computewallInt[3][2]);
-            sortedComputewall.add(computewallInt[3][3]);
-            sortedComputewall.add(computewallInt[3][3]);
-            sortedComputewall.add(computewallInt[4][4]);
-            sortedComputewall.add(computewallInt[4][1]);
-            sortedComputewall.add(computewallInt[4][2]);
-            sortedComputewall.add(computewallInt[4][3]);
-            Object[] sortedComputeWallInt = sortedComputewall.toArray();
-            Arrays.sort(sortedComputeWallInt);
-            if (sortedComputewall.contains(0) | !sortedComputewall.contains(1) | !sortedComputewall.contains(2) | !sortedComputewall.contains(3) | !sortedComputewall.contains(4) |
-                    !sortedComputewall.contains(5) | !sortedComputewall.contains(6) | !sortedComputewall.contains(7) | !sortedComputewall.contains(8) | !sortedComputewall.contains(9) |
-                    !sortedComputewall.contains(10) | !sortedComputewall.contains(11) | !sortedComputewall.contains(12) | !sortedComputewall.contains(13) | !sortedComputewall.contains(14) |
-                    !sortedComputewall.contains(15)) {
-                uniqueCheck = true;
-            }
-        } while (uniqueCheck);
-    }
+        /*
+        count++;
+        if (count % 100000 == 0) {
+            System.out.println("count: " + count);
+        }
+        sortedComputewall.clear();
+        unique = false;
 
-    public void computewallValueReplace(int[][] array, int row, int position, int value) {
-        array[row][position] = value;
+        computewallValueReplace(computewallInt, 4, i, j);
+
+        //Rest of calculating
+        computewallInt[3][0] = Math.abs(computewallInt[4][0] - computewallInt[4][1]);
+        computewallInt[3][1] = Math.abs(computewallInt[4][1] - computewallInt[4][2]);
+        computewallInt[3][2] = Math.abs(computewallInt[4][2] - computewallInt[4][3]);
+        computewallInt[3][3] = Math.abs(computewallInt[4][3] - computewallInt[4][4]);
+        computewallInt[2][0] = Math.abs(computewallInt[3][0] - computewallInt[3][1]);
+        computewallInt[2][1] = Math.abs(computewallInt[3][1] - computewallInt[3][2]);
+        computewallInt[2][2] = Math.abs(computewallInt[3][2] - computewallInt[3][3]);
+        computewallInt[1][0] = Math.abs(computewallInt[2][0] - computewallInt[2][1]);
+        computewallInt[1][1] = Math.abs(computewallInt[2][1] - computewallInt[2][2]);
+        computewallInt[0][0] = Math.abs(computewallInt[1][0] - computewallInt[1][1]);
+        sortedComputewall.add(computewallInt[0][0]);
+        sortedComputewall.add(computewallInt[1][0]);
+        sortedComputewall.add(computewallInt[1][1]);
+        sortedComputewall.add(computewallInt[2][0]);
+        sortedComputewall.add(computewallInt[2][1]);
+        sortedComputewall.add(computewallInt[2][2]);
+        sortedComputewall.add(computewallInt[3][0]);
+        sortedComputewall.add(computewallInt[3][1]);
+        sortedComputewall.add(computewallInt[3][2]);
+        sortedComputewall.add(computewallInt[3][3]);
+        sortedComputewall.add(computewallInt[3][3]);
+        sortedComputewall.add(computewallInt[4][4]);
+        sortedComputewall.add(computewallInt[4][1]);
+        sortedComputewall.add(computewallInt[4][2]);
+        sortedComputewall.add(computewallInt[4][3]);
+        Object[] sortedComputeWallInt = sortedComputewall.toArray();
+        Arrays.sort(sortedComputeWallInt);
+        if (!sortedComputewall.contains(0) | !sortedComputewall.contains(1) | !sortedComputewall.contains(2) | !sortedComputewall.contains(3) | !sortedComputewall.contains(4) |
+                !sortedComputewall.contains(5) | !sortedComputewall.contains(6) | !sortedComputewall.contains(7) | !sortedComputewall.contains(8) | !sortedComputewall.contains(9) |
+                !sortedComputewall.contains(10) | !sortedComputewall.contains(11) | !sortedComputewall.contains(12) | !sortedComputewall.contains(13) |
+                !sortedComputewall.contains(14) | !sortedComputewall.contains(15)) {
+            unique = true;
+        }
+        System.out.println(unique);
+        */
     }
 
 }
